da9793378e5f25d21c7dfa30bb01f677d0ce704e
Galih Rivanto <galih.rivanto@gmail.com>
2025-04-27 22:54:40 +0700

    fix: alternative m2m with manual join relation

--- main.go
+++ main.go
@@ -14,13 +14,15 @@ import (
 )
 
 type Order struct {
-	ID     int64 `bun:",pk,autoincrement"`
-	ItemID int64
+	bun.BaseModel `bun:"table:orders,alias:orders"`
+	ID            int64 `bun:",pk,autoincrement"`
+	ItemID        int64
 }
 
 type Item struct {
-	ID      int64 `bun:",pk,autoincrement"`
-	OrderID int64
+	bun.BaseModel `bun:"table:items,alias:items"`
+	ID            int64 `bun:",pk,autoincrement"`
+	OrderID       int64
 }
 
 type OrderToItem struct {
@@ -30,25 +32,30 @@ type OrderToItem struct {
 }
 
 type OrderM2M struct {
-	Order `bun:",extend"`
+	bun.BaseModel `bun:"table:orders,alias:orders"`
+	ID            int64 `bun:",pk,autoincrement"`
+	ItemID        int64
 
 	// Order and Item in join:Order=Item are fields in OrderToItem model
-	Items []Item `bun:"m2m:order_to_items,join:Order=Item"`
+	Items []Item `bun:"rel:has-many"`
 }
 
 type ItemM2M struct {
-	Item `bun:",extend"`
+	bun.BaseModel `bun:"table:items,alias:items"`
+	ID            int64 `bun:",pk,autoincrement"`
+	OrderID       int64
 
 	// Order and Item in join:Order=Item are fields in OrderToItem model
-	Orders []Order `bun:"m2m:order_to_items,join:Item=Order"`
+	Orders []Order `bun:"rel:has-many"`
 }
 
-type OrderToItemM2M struct {
-	OrderToItem `bun:",extend"`
-
-	Order *Order `bun:"rel:belongs-to,join:order_id=id"`
-	Item  *Item  `bun:"rel:belongs-to,join:item_id=id"`
-}
+// type OrderToItemM2M struct {
+// 	bun.BaseModel `bun:"table:order_to_items,alias:order_to_items_m2m"`
+// 	OrderID       int64     `bun:",pk"`
+// 	Order         *OrderM2M `bun:"rel:belongs-to,join:order_id=id"`
+// 	ItemID        int64     `bun:",pk"`
+// 	Item          *ItemM2M  `bun:"rel:belongs-to,join:item_id=id"`
+// }
 
 func main() {
 	ctx := context.Background()
@@ -69,7 +76,7 @@ func main() {
 
 	// Register many to many model so bun can better recognize m2m relation.
 	// This should be done before you use the model for the first time.
-	db.RegisterModel((*OrderToItemM2M)(nil))
+	// db.RegisterModel((*OrderToItemM2M)(nil))
 
 	// concurrently, bun does not support m2m relation for models with same name.
 	wg := sync.WaitGroup{}
@@ -107,7 +114,7 @@ func main() {
 }
 
 func selectUsingHasMany(ctx context.Context, db *bun.DB) error {
-	model := []OrderToItemM2M{}
+	model := []OrderToItem{}
 	if err := db.NewSelect().
 		Model(&model).
 		Join("JOIN orders").
@@ -124,13 +131,17 @@ func selectUsingHasMany(ctx context.Context, db *bun.DB) error {
 
 func selectUsingM2M(ctx context.Context, db *bun.DB) error {
 	// try to re-register before using in query
-	db.RegisterModel((*OrderToItemM2M)(nil))
+	// db.RegisterModel((*OrderToItemM2M)(nil))
 
 	order := new(OrderM2M)
 	if err := db.NewSelect().
 		Model(order).
-		Relation("Items").
-		Order("order.id ASC").
+		Relation("Items", func(sq *bun.SelectQuery) *bun.SelectQuery {
+			sq.Join("JOIN order_to_items").
+				JoinOn("order_to_items.item_id = items.id")
+			return sq
+		}).
+		Order("orders.id ASC").
 		Limit(1).
 		Scan(ctx); err != nil && !errors.Is(err, sql.ErrNoRows) {
 		return err
@@ -141,9 +152,11 @@ func selectUsingM2M(ctx context.Context, db *bun.DB) error {
 	order = new(OrderM2M)
 	if err := db.NewSelect().
 		Model(order).
-		Relation("Items", func(q *bun.SelectQuery) *bun.SelectQuery {
-			q = q.OrderExpr("item.id DESC")
-			return q
+		Relation("Items", func(sq *bun.SelectQuery) *bun.SelectQuery {
+			sq.Join("JOIN order_to_items").
+				JoinOn("order_to_items.item_id = items.id").
+				OrderExpr("items.id DESC")
+			return sq
 		}).
 		Limit(1).
 		Scan(ctx); err != nil && !errors.Is(err, sql.ErrNoRows) {
@@ -155,8 +168,12 @@ func selectUsingM2M(ctx context.Context, db *bun.DB) error {
 	item := new(ItemM2M)
 	if err := db.NewSelect().
 		Model(item).
-		Relation("Orders").
-		Order("item.id ASC").
+		Relation("Orders", func(sq *bun.SelectQuery) *bun.SelectQuery {
+			sq.Join("JOIN order_to_items").
+				JoinOn("order_to_items.order_id = orders.id")
+			return sq
+		}).
+		Order("items.id ASC").
 		Limit(1).
 		Scan(ctx); err != nil && !errors.Is(err, sql.ErrNoRows) {
 		return err
@@ -170,7 +187,7 @@ func createSchema(ctx context.Context, db *bun.DB) error {
 	models := []interface{}{
 		(*Order)(nil),
 		(*Item)(nil),
-		(*OrderToItemM2M)(nil),
+		(*OrderToItem)(nil),
 	}
 	for _, model := range models {
 		if _, err := db.NewCreateTable().Model(model).Exec(ctx); err != nil {
